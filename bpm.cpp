98628cd57d6eec8d0493d8d515c74b7d5ebdca4e
Varad Kshirsagar <f20170141@hyderabad.bits-pilani.ac.in>
2020-04-23 01:18:10 +0530

    Add files via upload

---
+++ bpm.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long li;
+
+bool bpm(vector<vector<bool>> &adj,li u,vector<bool> &seen, vector<li> &matchR){
+	li M = adj.size();
+	li N = adj[0].size();
+
+	for(li v = 0; v < N; v++){
+		if(adj[u][v] && !seen[v]){
+			seen[v] = true;
+
+			if(matchR[v] < 0 || bpm(adj,matchR[v],seen,matchR)){
+				matchR[v] = u;
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+int maxBPM(vector<vector<bool>> &adj){
+	li M = adj.size();
+	li N = adj[0].size();
+	vector<li> matchR(N,-1);
+	li result = 0;
+	for(li u = 0;u < M;u++){
+		vector<bool> seen(N,false);
+		if(bpm(adj,u,seen,matchR))
+			result++;
+	}
+	for (int i = 0; i < N; ++i){
+		if(matchR[i] != -1)
+			cout<<matchR[i]<<"->"<<i<<endl;
+	}
+	return result;
+}
+
+int main(){
+	auto total_start = chrono::steady_clock::now();
+
+	li E,V1,V2;
+	cin>>E>>V1>>V2;
+	
+	vector<vector<bool>> adj(V1,vector<bool> (V2,false));
+	li aux1,aux2;
+	
+	for (li i = 0; i < E; ++i){
+		cin>>aux1>>aux2;
+		adj[aux1-1][aux2-1] = 1;
+	}
+	
+	cout<<"---------MATCHINGS---------"<<endl;
+	auto algo_start = chrono::steady_clock::now();
+	cout<<maxBPM(adj)<<endl;
+	auto total_end = chrono::steady_clock::now();
+
+	auto total_time = chrono::duration_cast<chrono::milliseconds>(total_end - total_start).count();
+	auto algo_time = chrono::duration_cast<chrono::milliseconds>(total_end - algo_start).count();
+	
+	cout<<"TOTAL TIME: "<<(1.00*total_time/1000)<<" seconds"<<endl;
+	cout<<"ALGORITHM TIME: "<<(1.00*algo_time/1000)<<" seconds"<<endl;
+	return 0;
+}
